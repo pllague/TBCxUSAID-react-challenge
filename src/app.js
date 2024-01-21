ae595ead8e64384cc4e77ca1ffe2d74a7bcb051a
Guga Karalashvili <guga.karalashvili@gmail.com>
2024-01-21 15:59:06 +0400

    add header scroll functionality on mobile version

--- src/app.js
+++ src/app.js
@@ -7,3 +7,33 @@ menuBurger.addEventListener('click', () => {
     menuBurger.classList.toggle('h-[33.23px]');
     menuBurger.classList.toggle('w-[33.23px]');
 });
+
+
+// Hide/Appear header while scrolling on mobile version
+const header = document.querySelector('header');
+let lastScrollPosition = 0;
+
+// window on load
+window.addEventListener('load', () => {
+    window.addEventListener('scroll', () => {
+        const currentScroll = window.scrollY; // Get current scroll position
+
+        // Set data position based on scroll
+        if (currentScroll > 0) {
+            header.dataset.position = 'center';
+        } else {
+            header.dataset.position = 'top';
+        }
+
+        // Check if user is scrolling up or down
+        if (window.innerWidth < 1024) {
+            if (lastScrollPosition > currentScroll && currentScroll > 0) {
+                header.style.transform = 'translateY(-100%)';
+            } else {
+                header.style.transform = 'translateY(0)';
+            }
+            lastScrollPosition = currentScroll;
+        }
+    });
+
+});
